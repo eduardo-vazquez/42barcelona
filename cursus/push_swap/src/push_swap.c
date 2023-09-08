8622a28dc399687b51db3b92e39e241f5a0d531d
Eduardo Maria Vazquez Rojas <evazquez@cbr5s6.42barcelona.com>
2023-09-08 16:58:10 +0200

    se corrige makefiles y se agrega argv

--- cursus/push_swap/src/push_swap.c
+++ cursus/push_swap/src/push_swap.c
@@ -6,14 +6,21 @@
 /*   By: evazquez <evazquez@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:15:43 by evazquez          #+#    #+#             */
-/*   Updated: 2023/09/07 18:39:47 by evazquez         ###   ########.fr       */
+/*   Updated: 2023/09/08 16:56:58 by evazquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	printf("hola mundo");
+	int	i;
+
+	i = 0;
+	if (argc < 2)
+		printf("Error\n");
+	while (i < argc)
+		printf("numero %s\n", argv[i++]);
+	printf("hola mundo\n");
 	return (0);
 }
