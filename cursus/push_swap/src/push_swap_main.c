f8bde0263dfb44e0745663dff8910e044c2c35c1
Eduardo Maria Vazquez Rojas <evazquez@cbr4s6.42barcelona.com>
2023-09-29 19:30:20 +0200

    h

--- cursus/push_swap/src/push_swap_main.c
+++ cursus/push_swap/src/push_swap_main.c
@@ -6,7 +6,7 @@
 /*   By: evazquez <evazquez@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:35:43 by evazquez          #+#    #+#             */
-/*   Updated: 2023/09/28 16:00:14 by evazquez         ###   ########.fr       */
+/*   Updated: 2023/09/29 19:29:26 by evazquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,18 @@ void	ft_load_nodes(node **head, char **argv, int argc)
 
 int	main(int argc, char **argv)
 {
-	node *stack_a;
-	node *stack_b;
+	node *head_stack_a;
+	node *head_stack_b;
 
 	if (argc < 2)
 		return (-1);
-	stack_b = (node *)malloc(sizeof(node));
-	ft_load_nodes(&stack_a, argv, argc);
+	head_stack_a = (node *)malloc(sizeof(node));
+	head_stack_b = (node *)malloc(sizeof(node));
+	ft_load_nodes(&head_stack_a, argv, argc);
 
 	//ELIMINAR
-	node	*tmp = stack_b;
+	node	*tmp; 
+	tmp = head_stack_a;
 	while (tmp != NULL)
 	{
 		printf("value: %d, index: %d\n", tmp->value, tmp->index);
